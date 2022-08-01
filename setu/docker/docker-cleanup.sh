dfdd0daf6a9cfe2b9fa823b5d1d17e3f15a85ece
Kiran Saha <kiransaha.588@gmail.com>
2022-08-02 00:51:43 +0530

    Update docker-cleanup.sh

--- setu/docker/docker-cleanup.sh
+++ setu/docker/docker-cleanup.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+
+echo "Docker container before cleanup ..."
+docker ps
+
+if [ -z ${CONTAINER_NAME} ]; then
+    echo "No docker cleanup, conntainer name is not passed"
+else
+    echo "Container name recieved from env : ${CONTAINER_NAME}, cleaning only that container ..."
+    docker stop $(docker ps -aqf "name=^${CONTAINER_NAME}$")
+    docker rm $(docker ps -aqf "name=^${CONTAINER_NAME}$")
+    docker rmi $(docker images "${CONTAINER_NAME}" -q)
+fi
+
+echo "After Cleanup ..."
+docker ps
+echo "Done"
+
